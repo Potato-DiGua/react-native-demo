a01b01a010bf7c0600caa14f7bb479278201e163
zzl <86543402@qq.com>
2020-08-13 20:24:09 +0800

    init

---
+++ src/MovieItem.tsx
@@ -0,0 +1,84 @@
+import React, { Component } from "react";
+import {
+    View,
+    Image,
+    Text,
+    StyleSheet,
+    TouchableHighlight,
+    Alert,
+    Platform
+} from "react-native";
+
+export interface MovieItemState {
+    mtime: string;
+    title: string;
+    digest: string;
+    imgsrc: string;
+    url: string;
+    source: string;
+}
+
+export interface MovieItemProps {
+    data: MovieItemState;
+}
+export default class MovieItem extends Component<MovieItemProps, MovieItemState>{
+    constructor(props: MovieItemProps) {
+        super(props)
+        if (this.props != null && this.props.data != null) {
+            this.state = this.props.data;
+        }
+    }
+    onClick() {
+        Alert.alert("Tips", this.state.title + "\n系统版本：" + Platform.OS + " " + Platform.Version);
+    }
+    render() {
+        if (this.state != null) {
+            return (
+                <TouchableHighlight onPress={() => this.onClick()} style={styles.btn}>
+                    <View style={styles.container}>
+                        <Text style={styles.title}>{this.state.title}</Text>
+                        <View style={styles.content}>
+                            <Image source={{ uri: this.state.imgsrc }} style={styles.thumbnail} resizeMode={"cover"} />
+                            <View style={styles.rightContainer}>
+                                <Text style={styles.year}>{this.state.digest}</Text>
+                            </View>
+                        </View>
+                    </View>
+                </TouchableHighlight>
+            );
+        } else {
+            return <></>
+        }
+
+    }
+}
+const styles = StyleSheet.create({
+    btn: {
+        marginBottom: 10
+    },
+    container: {
+        backgroundColor: '#F5FCFF',
+        padding: 10
+    },
+    content: {
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+    },
+    rightContainer: {
+        flex: 1,
+        marginStart: 20,
+        justifyContent: 'flex-start'
+    },
+    thumbnail: {
+        width: 150,
+        height: 75
+    },
+    title: {
+        fontSize: 20,
+        marginBottom: 8
+    },
+    year: {
+
+    }
+});
